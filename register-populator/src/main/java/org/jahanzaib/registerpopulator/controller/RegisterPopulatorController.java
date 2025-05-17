1523cb143e723199e3f0b843e3c09fa2b59d3a5b
Jahan Alam <jahanzaybalam@gmail.com>
2025-05-17 18:11:56 +0100

    Use env variables for zoom api details

--- register-populator/src/main/java/org/jahanzaib/registerpopulator/controller/RegisterPopulatorController.java
+++ register-populator/src/main/java/org/jahanzaib/registerpopulator/controller/RegisterPopulatorController.java
@@ -2,7 +2,6 @@ package org.jahanzaib.registerpopulator.controller;
 
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import jakarta.validation.Valid;
-import java.time.format.DateTimeFormatter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.jahanzaib.registerpopulator.dto.PopulateRequestDTO;
@@ -23,7 +22,8 @@ public class RegisterPopulatorController {
 
   @RateLimiter(name = "registerPopulatorApiLimiter")
   @GetMapping("/populate")
-  public ResponseEntity<?> populateAttendancesAndAbsences(@Valid @RequestBody PopulateRequestDTO populateRequest) {
+  public ResponseEntity<?> populateAttendancesAndAbsences(
+      @Valid @RequestBody PopulateRequestDTO populateRequest) {
     log.info("Running application for date: {}", populateRequest.getDateToUpdate());
     attendanceProcessor.processAttendanceAndAbsences(populateRequest);
     return ResponseEntity.ok().build();
